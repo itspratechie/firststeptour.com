0b8ab2335eb6223cf1ffbd6aeda923d8349c4562
Prateek Kumar <annupachauri6@gmail.com>
2024-09-10 23:27:18 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,128 @@
+<?php
+        // <!-- footer area start -->
+        "<footer class="main-footer footer-two bgp-bottom bgc-black rel z-15 pt-100 pb-115" style="background-image: url(assets/images/backgrounds/footer-two.png); margin-bottom: -100px;">
+            <div class="widget-area">
+                <div class="container">
+                    <div class="row row-cols-xxl-5 row-cols-xl-4 row-cols-md-3 row-cols-2">
+                        <div class="col col-small" data-aos="fade-up" data-aos-duration="1500" data-aos-offset="50">
+                            <div class="footer-widget footer-text">
+                                <div class="footer-logo mb-40">
+                                    <a href="index.html"><img src="assets/images/logos/fstlogo.png" height="60px" alt="Logo"></a>
+                                </div>
+                                <div class="footer-map">
+                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d113579.79425687161!2d77.89761132816231!3d27.17615042454691!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39740d857c2f41d9%3A0x784aef38a9523b42!2sAgra%2C%20Uttar%20Pradesh!5e0!3m2!1sen!2sin!4v1725777058663!5m2!1sen!2sin" width="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                </div>
+                                <!--Social Icons-->
+                                <hr>
+                                <div class="social-style-one">
+                                    <a href="contact.html"><i class="fab fa-twitter"></i></a>
+                                    <a href="contact.html"><i class="fab fa-facebook-f"></i></a>
+                                    <a href="contact.html"><i class="fab fa-instagram"></i></a>
+                                    <a href="#"><i class="fab fa-linkedin"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col col-small" data-aos="fade-up" data-aos-delay="50" data-aos-duration="1500" data-aos-offset="50">
+                            <div class="footer-widget footer-links ms-sm-5">
+                                <div class="footer-title">
+                                    <h5>Services</h5>
+                                </div>
+                                <ul class="list-style-three">
+                                    <li><a href="destination-details.html">Best Tour Guide</a></li>
+                                    <li><a href="destination-details.html">Tour Booking</a></li>
+                                    <li><a href="destination-details.html">Hotel Booking</a></li>
+                                    <li><a href="destination-details.html">Ticket Booking</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col col-small" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1500" data-aos-offset="50">
+                            <div class="footer-widget footer-links ms-md-4">
+                                <div class="footer-title">
+                                    <h5>Company</h5>
+                                </div>
+                                <ul class="list-style-three">
+                                    <li><a href="about.html">About Company</a></li>
+                                    <li><a href="faqs.html">FAQs</a></li>
+                                    <li><a href="gellery-grid.html">Gallery</a></li>
+                                    <li><a href="contact.html">Contact Us</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col col-small" data-aos="fade-up" data-aos-delay="150" data-aos-duration="1500" data-aos-offset="50">
+                            <div class="footer-widget footer-links ms-lg-4">
+                                <div class="footer-title">
+                                    <h5>Tours</h5>
+                                </div>
+                                <ul class="list-style-three">
+                                    <li><a href="golden-triangle-tours.html">Golden Triangle Tours</a></li>
+                                    <li><a href="same-day-tours.html">Same Day Tours</a></li>
+                                    <li><a href="taj-mahal-tours.html">Taj Mahal Tours</a></li>
+                                    <li><a href="special-tours.html">Special Tours</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col col-md-6 col-10 col-small" data-aos="fade-up" data-aos-delay="200" data-aos-duration="1500" data-aos-offset="50">
+                            <div class="footer-widget footer-contact">
+                                <div class="footer-title">
+                                    <h5>Get In Touch</h5>
+                                </div>
+                                <ul class="list-style-one">
+                                    <li><i class="fal fa-map-marked-alt"></i> 3005, Luxe Paradise, Taj Nagri Phase ll, Fatehabad Road, Agra(282001)</li>
+                                    <li><i class="fal fa-envelope"></i> <a href="mailto:info@firststeptour.com">info@firststeptour.com</a></li>
+                                    <li><i class="fal fa-envelope"></i> <a href="mailto:firststeptour01@gmail.com">firststeptour01@gmail.com</a></li>
+                                    <li><i class="fal fa-phone-volume"></i> <a href="callto:+918006128006">+91 8006128006</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom bg-transparent pt-20 pb-5">
+                <div class="container">
+                    <div class="row">
+                       <div class="col-lg-7">
+                            <div class="copyright-text text-center text-lg-start">
+                                <p>@Copy 2024 <a href="index.html">First Step Tour</a>, All rights reserved.  Developed by <a href="index.html">Pratechie Technologies</a></p>
+                            </div>
+                       </div>
+                       <div class="col-lg-5 text-center text-lg-end">
+                           <ul class="footer-bottom-nav">
+                               <li><a href="about.html">Terms</a></li>
+                               <li><a href="about.html">Privacy Policy</a></li>
+                               <li><a href="about.html">Legal notice</a></li>
+                               <li><a href="about.html">Accessibility</a></li>
+                           </ul>
+                       </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- footer area end -->
+
+    </div>
+    <!--End pagewrapper-->
+   
+    
+    <!-- Jquery -->
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Appear Js -->
+    <script src="assets/js/appear.min.js"></script>
+    <!-- Slick -->
+    <script src="assets/js/slick.min.js"></script>
+    <!-- Magnific Popup -->
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <!-- Nice Select -->
+    <script src="assets/js/jquery.nice-select.min.js"></script>
+    <!-- Image Loader -->
+    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+    <!-- Skillbar -->
+    <script src="assets/js/skill.bars.jquery.min.js"></script>
+    <!-- Isotope -->
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+    <!--  AOS Animation -->
+    <script src="assets/js/aos.js"></script>
+    <!-- Custom script -->
+    <script src="assets/js/script.js"></script>
+?>
